e75a0a76fbd183d123f05b15cc48ef809accc7f2
zulujumper <zulujumper@gmail.com>
2023-04-03 09:44:31 -0700

    Function Declarations

---
+++ Week3/3.1/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,40 @@
+// TODO: using the function expressions below, refactor them into function declarations
+// const greet = function() {
+    // console.log("Hello!");
+//   };
+
+//   const threeModTwo = function() {
+//     console.log(3 % 2);
+//   };
+
+// let age = 18;
+// const checkDrivingAge = function() {
+// if (age >= 16) {
+// console.log(true);
+// } else {
+//     console.log(false);
+// }
+// };
+
+function greet() {
+    console.log("Hello!");
+};
+
+function threeModTwo() {
+    console.log(3 % 2);
+};
+
+let age = 18;
+function checkDrivingAge() {
+    if (age >= 16) {
+        console.log(true);
+        } else {
+            console.log(false);
+        }
+};
+
+
+
+
+
+
